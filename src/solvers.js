bd949e6e7cb4951fac6833e4efeaad9b5473aad8
Adam Brent <adam.brent@gmail.com>
2020-12-03 17:38:00 -0800

    Adds latest work on findNRooksSolution.

--- src/solvers.js
+++ src/solvers.js
@@ -13,14 +13,84 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
+// input number
+// output matrix: array of arrays (Board object)
+// constraints: none
+// edge cases: none
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  //var solution = undefined;
+  // assign solution to new Board of size n
+  var solution = new Board({n: n});
+  // define number of rooks placed as 0
+  var rooks = 0;
+  // define an inner set function addRook(row, column)
+  var addRook = function (row, col) {
+    // check if rooks < n
+    if (rooks === n) {
+      // set current house to 1
+      console.log('solution before set', solution);
+      console.log('solution after set', solution);
+      return;
+    }
+    var currentRow = solution.get(row);
+    currentRow[col] = 1;
+    solution.set(row, currentRow);
 
+    // check if current row has row conflict
+    if (solution.hasRowConflictAt(row)) {
+      // if it does, set current house to 0
+      currentRow[col] = 0;
+      solution.set(row, currentRow);
+      // invoke addRook(next row, same column)
+      addRook(row+1, col);
+    }
+
+    // check if current column has column conflict
+    if (solution.hasColConflictAt(col)) {
+      // if it does, set current house to 0
+      currentRow[col] = 0;
+      solution.set(row, currentRow);
+      // invoke addRook(same row, next column)
+      addRook(row, col+1);
+
+    }
+
+
+    // increment rooks
+    rooks +=1;
+    console.log('rooks', rooks);
+
+  }
+
+  //define row and col and set them to 0
+  var row = 0;
+  var col = 0;
+  // iterate from 0 to n (i)
+  for (var i = 0; i < n; i++) {
+    // invoke addRooks(row, column)
+    addRook(row, col);
+    // col++
+    col ++;
+
+  }
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  console.log('solution', solution);
+
+  delete solution.attributes.n;
+  solution = solution.attributes;
+  var testArr = [];
+  for (var key in solution) {
+    testArr.push(solution[key]);
+  }
+  console.log(testArr);
+  return testArr;
+
 };
+
+//test :
+
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
